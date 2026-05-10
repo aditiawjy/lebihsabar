f822d76824306e8cac3504fee6e99b90518a83bf
Adit <rva.ant@gmail.com>
2026-05-10 14:14:51 +0700

    Update lebihsabar project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.php',
+    './bridgebrowser/**/*.php',
+    './live-scraper/**/*.php',
+    './msports-parser/**/*.php'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
